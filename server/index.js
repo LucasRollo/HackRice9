0d9963926d247e340bc6d5416dc0deb2fc6160f8
AkshatJain1 <respectthegame23@gmail.com>
2019-09-21 05:07:53 -0500

    abstracted routes file

--- server/index.js
+++ server/index.js
@@ -35,67 +35,8 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 });
 
-let User = require("models/user.model");
-
-
-//ROUTES START
-
-app.get("/", function(req,res){
-  if (req.isAuthenticated()){
-    res.render("/");
-  } else {
-    res.redirect("/login");
-  }
-});
-
-
-app.post("/register", function(req, res){
-  //only adds username and password to new User record (encrypted so we have to keep it like this for now)
-  User.register({username: req.body.username}, req.body.password, function(err, user){
-    if (err) {
-      console.log(err);
-      res.redirect("/register");
-    } else {
-      passport.authenticate("local")(req, res, function(){
-        //!Append Phone, Fname, and Lname to user record!
-        res.redirect("/");
-      });
-    }
-  });
-
-});
-
-
-
-app.post("/login", function(req, res){
-
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
-
-  req.login(user, function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      passport.authenticate("local")(req, res, function(){
-        res.redirect("/");
-      });
-    }
-  });
-
-});
-
-
-
-
-
-
-
-
-
-
-
+const router = require('router')
+app.use("/", router);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
