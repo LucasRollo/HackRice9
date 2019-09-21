3937313b166cef8e77f6097b1213297f1d3a9b49
Lucas Rollo <charleslucasrollo@gmail.com>
2019-09-21 07:48:37 -0500

    Fix registration

--- server/router.js
+++ server/router.js
@@ -1,5 +1,6 @@
-const router = require('express').Router();
+//jshint esversion:6
 
+const router = require('express').Router();
 let User = require("./models/users.model");
 
 //Login/Register
@@ -15,12 +16,23 @@ router.route('/').get((req,res) => {
 
 router.route("/register").post((req, res) => {
   //only adds username and password to new User record (encrypted so we have to keep it like this for now)
+  console.log(req);
   User.register({username: req.body.username}, req.body.password, function(err, user){
     if (err) {
       console.log(err);
       res.redirect("/register");
     } else {
       passport.authenticate("local")(req, res, function(){
+
+        // User.findOneAndUpdate({ "_id": req.user.id }, { "$set": { "f_name": name, "l_name": lname,"genre": genre, "author": author, "similar": similar}}).exec(function(err, book){
+        //   if(err) {
+        //     console.log(err);
+        //     res.status(500).send(err);
+        //   } else {
+        //     res.status(200).send(book);
+        //   }
+        // });
+
         //!Append Phone, Fname, and Lname to user record!
         res.redirect("/");
       });
